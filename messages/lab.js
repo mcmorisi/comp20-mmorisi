43640d0379126009481e7261533d13a4a1ef10fa
Michael Morisi <mcmorisi@gmail.com>
2016-03-10 16:08:12 -0500

    Fix js

--- messages/lab.js
+++ messages/lab.js
@@ -1,21 +1,21 @@
 var xmlhttp = new XMLHttpRequest();
 var url = "data.json";
+xmlhttp.open("GET", url, true);
+
 
 xmlhttp.onreadystatechange = function() {
         if(xmlhttp.readyState == 4 && xmlhttp.status = 200) {
                 data = JSON.parse(xmlhttp.responseText);
+                printMsg(data);
         }
 };
-
-xmlhttp.open("GET", url, true);
-xmlhttp.send();
 
 function printMsg(arr) {
         var out = "";
         var output = getElementById("messages").innerHTML;
         for(i = 0; i < arr.length, i++) {
-                out += "<span class='msg'>" + arr[i].content +
-                        " - " + arr[i].username + "</span><br>";
+                out += "<span class='msg'>" + arr[i]["content"] +
+                        " - " + arr[i]["username"] + "</span><br>";
         }
 
         output = out;
