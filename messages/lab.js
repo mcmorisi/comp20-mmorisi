1402c8bdcd99a88a1a1d6d0852a6017cb25cd2a1
Michael Morisi <mcmorisi@gmail.com>
2016-03-10 15:49:04 -0500

    Work on lab

--- messages/lab.js
+++ messages/lab.js
@@ -1,1 +1,22 @@
-// Your JavaScript goes here...
+var xmlhttp = new XMLHttpRequest();
+var url = "data.json";
+
+xmlhttp.onreadystatechange = function() {
+        if(xmlhttp.readyState == 4 && xmlhttp.status = 200) {
+                data = JSON.parse(xmlhttp.responseText);
+        }
+};
+
+xmlhttp.open("GET", url, true);
+xmlhttp.send();
+
+function printMsg(arr) {
+        var out = "";
+        var output = getElementById("messages").innerHTML;
+        for(i = 0; i < arr.length, i++) {
+                out += "<span class='msg'>" + arr[i].content +
+                        " - " + arr[i].username + "</span><br>";
+        }
+
+        output = out;
+}
